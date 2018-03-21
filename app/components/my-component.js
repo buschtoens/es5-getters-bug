fc1a846ee352689d20591212fb4d455b54e80137
Jan Buschtöns <jan@buschtoens.me>
2018-03-21 14:37:09 +0100

    add other demo

--- app/components/my-component.js
+++ app/components/my-component.js
@@ -1,8 +1,10 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+// import { service } from '@ember-decorators/service';
 import { reads } from '@ember-decorators/object/computed';
 
 export default Component.extend({
+  // @service store: null
   store: service(),
 
   @reads('store') storeAlias: null
